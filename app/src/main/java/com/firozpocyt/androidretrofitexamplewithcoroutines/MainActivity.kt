14971bcbca85d8d178c30917c5a19e6719558466
Firoz Shaikh <gitfiroz695@gmail.com>
2023-08-16 16:39:12 +0530

    Android Retrofit Tutorial With Coroutines

---
+++ app/src/main/java/com/firozpocyt/androidretrofitexamplewithcoroutines/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.firozpocyt.androidretrofitexamplewithcoroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val quotesAPI = RetrofitHelper.getInstance().create(QuotesAPI::class.java)
+
+        GlobalScope.launch {
+            val result = quotesAPI.getQuotes(1)
+            if (result !== null){
+                val quoteList = result.body()
+                quoteList?.results?.forEach {
+                    Log.d("FirozPOC", it.content)
+                }
+
+            }
+        }
+    }
+}
